73aa36212aa7a52a2f736b2cbd278f597432cc51
Cristiano <cristiano.16bits@gmail.com>
2022-06-22 23:01:02 -0300

    Classe do banco de dados

---
+++ database.py
@@ -0,0 +1,33 @@
+import sqlite3
+
+class Database:
+    def __init__(self):
+        self.conn = sqlite3.connect('data.db',check_same_thread=False)
+        self.cur = self.conn.cursor()
+        self.DropTable()
+        self.createTable()
+
+    def createTable(self):
+        self.cur.execute('''
+            CREATE TABLE person(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name varchar(60) NOT NULL,
+                latitude varchar(255) NOT NULL,
+                longitude varchar(255) NOT NULL,
+                date DATETIME
+            );''')
+
+    def DropTable(self):
+        self.cur.execute('''DROP TABLE IF EXISTS person;''')
+    
+    def insertData(self,name,latitude,longitude):
+        self.cur.execute('''
+            INSERT INTO person (name,latitude,longitude)
+            VALUES ('{}','{}','{}')
+        '''.format(name,latitude,longitude)
+        )
+        
+        self.commit()
+    
+    def commit(self):
+        self.conn.commit()
